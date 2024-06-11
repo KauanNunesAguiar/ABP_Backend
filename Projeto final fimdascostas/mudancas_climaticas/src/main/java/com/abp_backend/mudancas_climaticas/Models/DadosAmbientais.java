cbd163aba540aead154efb7cdab93d1a361eaff4
Kauan Nunes Aguiar <133172749+KauanNunesAguiar@users.noreply.github.com>
2024-06-10 21:38:24 -0300

    Add files via upload

---
+++ Projeto final fimdascostas/mudancas_climaticas/src/main/java/com/abp_backend/mudancas_climaticas/Models/DadosAmbientais.java
@@ -0,0 +1,59 @@
+package com.abp_backend.mudancas_climaticas.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class DadosAmbientais {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    private String location;
+    private double temperature;
+    private double humidity;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+}
